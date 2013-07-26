0293213ae7d5f5ce05afcfbc6280a21485bb45af
Anthony Nguyen <anknguyen@gmail.com>
2013-07-26 11:05:31 -0400

    Added com_nosplash because it's actually useful. Also removed remnants of cignore.

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -1335,7 +1335,7 @@ void CL_Connect_f( void ) {
   // server connection string
   Cvar_Set( "cl_currentServerAddress", server );
 
-  CL_IgnoreClear();
+  // CL_IgnoreClear();
 }
 
 #define MAX_RCON_MESSAGE 1024
