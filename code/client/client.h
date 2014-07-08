1714de147bfa33d9cbfe9c085952dedd36ebd532
Anthony Nguyen <anknguyen@gmail.com>
2014-07-08 18:06:27 -0400

    Moved console_t struct back into cl_console.c

--- code/client/client.h
+++ code/client/client.h
@@ -488,40 +488,12 @@ qboolean CL_UpdateVisiblePings_f( int source );
 // console
 //
 
-#define	NUM_CON_TIMES 4
-
-#define		CON_TEXTSIZE	32768
-typedef struct {
-	qboolean	initialized;
-
-	short	text[CON_TEXTSIZE];
-	int		current;		// line where next message will be printed
-	int		x;				// offset in current line for next print
-	int		display;		// bottom of console displays this line
-
-	int 	linewidth;		// characters across screen
-	int		totallines;		// total lines in console scrollback
-
-	float	xadjust;		// for wide aspect screens
-	float   yadjust;
-
-	float	displayFrac;	// aproaches finalFrac at scr_conspeed
-	float	finalFrac;		// 0.0 to 1.0 lines of console to display
-
-	int		vislines;		// in scanlines
-
-	int		times[NUM_CON_TIMES];	// cls.realtime time the line was generated
-								// for transparent notify lines
-	vec4_t color;
-} console_t;
-
 extern	cvar_t	*con_prompt;
 extern	cvar_t	*con_promptColour;
 extern	cvar_t	*con_timePrompt;
 extern	cvar_t	*con_tabs;
 void Con_DrawCharacter (int cx, int line, int num);
 
-void Con_CheckResize (console_t *console);
 void Con_Init (void);
 void Con_Clear_f (void);
 void Con_ToggleConsole_f (void);
