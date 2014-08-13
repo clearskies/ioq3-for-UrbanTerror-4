91f6271a814166081f0d898d4d7c635af523642f
Anthony Nguyen <anknguyen@gmail.com>
2014-08-13 11:42:59 -0400

    Added SV_LogPrintf (@FenixXx)

--- code/server/server.h
+++ code/server/server.h
@@ -338,6 +338,9 @@ extern  cvar_t  *sv_auth_engine;
 //
 // sv_main.c
 //
+
+void QDECL SV_LogPrintf(const char *fmt, ...);
+
 void SV_FinalMessage (char *message);
 void QDECL SV_SendServerCommand( client_t *cl, const char *fmt, ...);
 
