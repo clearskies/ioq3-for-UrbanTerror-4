f4fd68c2d2ac22cc773d06da46f743d6ceb56896
Anthony Nguyen <anknguyen@gmail.com>
2014-08-18 09:38:05 -0400

    Updated sound system warning string

--- code/client/snd_mem.c
+++ code/client/snd_mem.c
@@ -223,11 +223,11 @@ qboolean S_LoadSound( sfx_t *sfx )
 		return qfalse;
 
 	if ( info.width == 1 ) {
-		Com_DPrintf(S_COLOR_YELLOW "WARNING: %s is a 8 bit wav file\n", sfx->soundName);
+		Com_DPrintf(S_COLOR_YELLOW "WARNING: %s is an 8 bit sound file\n", sfx->soundName);
 	}
 
 	if ( info.rate != 22050 ) {
-		Com_DPrintf(S_COLOR_YELLOW "WARNING: %s is not a 22kHz wav file\n", sfx->soundName);
+		Com_DPrintf(S_COLOR_YELLOW "WARNING: %s is not a 22kHz sound file\n", sfx->soundName);
 	}
 
 	samples = Hunk_AllocateTempMemory(info.samples * sizeof(short) * 2);
