clearskies/ioq3-for-UrbanTerror-4

commit bbe1ac4f969c534983a8d07ac756b035af55059b

Added chat variable: $lasthitby

code/client/cl_main.c
@@ -205,19 +205,27 @@ void CL_AddReliableCommand( const char *cmd ) {
 	int   index;
 	char *s, *serverInfo;
 	char *redTeam, *blueTeam;
-	char *pName, *teamName, *oTeamName;
+	char *teamName, *oTeamName;
+	char *pName;
 	char *locName;
+	int lastHitByNum;
+	char *lastHitBy;
 	char health[4];
 
-	s = Z_Malloc(strlen(cmd) + 1);
-	Com_sprintf(s, strlen(cmd) + 1, "%s", cmd);
+	s = CopyString(cmd);
 
 	Com_sprintf(health, 4, "%i", cl.snap.ps.stats[0]);
+	s = replaceStr(s, "$hp", health);
+
 	pName = Info_ValueForKey(cl.gameState.stringData + cl.gameState.stringOffsets[544 + cl.snap.ps.clientNum], "n");
 	s = replaceStr(s, "$p", pName);
 
 	locName = cl.gameState.stringData + cl.gameState.stringOffsets[640 + cl.lastLocation];
 	s = replaceStr(s, "$loc", locName);
+
+	lastHitByNum = cl.snap.ps.persistant[PERS_ATTACKER];
+	lastHitBy = Info_ValueForKey(cl.gameState.stringData + cl.gameState.stringOffsets[544 + lastHitByNum], "n");
+	s = replaceStr(s, "$lasthitby", lastHitBy);
 
 	oTeamName = "Everyone";
 	serverInfo = cl.gameState.stringData + cl.gameState.stringOffsets[CS_SERVERINFO];
@@ -243,9 +251,6 @@ void CL_AddReliableCommand( const char *cmd ) {
 	} else {
 		teamName = "Spectator";
 	}
-
-	s = replaceStr(s, "$hp", health);
-	
 	s = replaceStr(s, "$team", teamName);
 	s = replaceStr(s, "$oteam", oTeamName);
 

commit 50d23e5e65dc0310fd371de8f3fb31d05612e4fa

Fixed bug introduced in bbe1ac4f that caused crashes
When killed by a non-player (lava, lemming, etc) without having been previously hit by a player, the id of the player who last hit you in the playerstate is 1022 or something similar. This would cause a segfault when the engine tried to access configstring 544 + 1022, which exceeds the configstring limit.

code/client/cl_main.c
@@ -229,7 +229,12 @@ void CL_AddReliableCommand( const char *cmd ) {
 	s = replaceStr(s, "$lastloc", locName);
 
 	lastHitByNum = cl.snap.ps.persistant[PERS_ATTACKER];
-	lastHitBy = Info_ValueForKey(cl.gameState.stringData + cl.gameState.stringOffsets[544 + lastHitByNum], "n");
+	
+	if (lastHitByNum < MAX_CLIENTS)
+		lastHitBy = Info_ValueForKey(cl.gameState.stringData + cl.gameState.stringOffsets[544 + lastHitByNum], "n");
+	else
+		lastHitBy = "World";
+
 	s = replaceStr(s, "$lasthitby", lastHitBy);
 
 	oTeamName = "Everyone";