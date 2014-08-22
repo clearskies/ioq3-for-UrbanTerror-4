2491b6b8e4fb6ebbf1204ddf02c3b272ba8871e2
Anthony Nguyen <anknguyen@gmail.com>
2014-08-22 18:30:41 -0400

    Saved password on the client

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -3198,7 +3198,7 @@ void CL_Init( void ) {
 	Cvar_Get ("handicap", "100", CVAR_USERINFO | CVAR_ARCHIVE );
 	Cvar_Get ("sex", "male", CVAR_USERINFO | CVAR_ARCHIVE );
 
-	Cvar_Get ("password", "", CVAR_USERINFO);
+	Cvar_Get ("password", "", CVAR_USERINFO | CVAR_ARCHIVE);
 
 	// cgame might not be initialized before menu is used
 	Cvar_Get ("cg_viewsize", "100", CVAR_ARCHIVE );
