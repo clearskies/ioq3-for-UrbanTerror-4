486e65ea27d063e4097c46ac204643ffb90edd6e
Anthony Nguyen <anknguyen@gmail.com>
2014-10-24 18:08:26 -0400

    Added fontImage loading and drawing

--- code/client/client.h
+++ code/client/client.h
@@ -330,9 +330,19 @@ typedef struct {
 	qhandle_t	charSetShader;
 	qhandle_t	whiteShader;
 	qhandle_t	consoleShader;
+
+	fontInfo_t  font;
+	qboolean    fontFont;
 } clientStatic_t;
 
 extern	clientStatic_t		cls;
+
+typedef enum {
+	ITEM_TEXTSTYLE_NORMAL,
+	ITEM_TEXTSTYLE_SHADOWED,
+	ITEM_TEXTSTYLE_SHADOWEDLESS
+} textStyle_t;
+
 
 //=============================================================================
 
@@ -553,6 +563,8 @@ void	SCR_DrawSmallChar( int x, int y, int ch );
 
 void	SCR_DrawStringExt( int x, int y, float size, const char *string, float *setColor, qboolean forceColor );
 void	SCR_DrawCondensedString( int x, int y, float size, const char *string, float *setColor, qboolean forceColor );
+
+void CL_DrawFontText(float x, float y, float scale, vec4_t color, const char *text, int style);
 
 //
 // cl_cin.c
